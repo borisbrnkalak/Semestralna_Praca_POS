e1560b74fee8d1c4f6237a2df5b600213119ede5
borisbrnkalak <borisbrnkalak@gmail.com>
2021-12-29 18:06:21 +0100

    Client_server_zaklad_1.0

--- registracia.c
+++ registracia.c
@@ -2,9 +2,16 @@
 // Created by user on 28. 12. 2021.
 //
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+
 
 void prihlas() {
     printf("===================\n");
@@ -12,13 +19,23 @@ void prihlas() {
     printf("===================\n");
     char* meno;
     char* heslo;
+    char buffMeno[128];
+    char buffHeslo[128];
+    bool prihlasilSa = false;
+
+
     printf("\n");
     printf("Zadajte meno: \n");
     scanf("%s", &meno);
     printf("Zadajte heslo: \n");
     scanf("%s", &heslo);
 
-    printf("Prihlasil sa pouzivatel: %s, s heslom %s\n", &meno, &heslo);
+    if(prihlasilSa) {
+        printf("Prihlasil sa pouzivatel: %s, s heslom %s\n", &meno, &heslo);
+    } else {
+        printf("Tento uzivatel neexistuje, skuste to znova.\n");
+        prihlas(socket);
+    }
 
 }
 
@@ -30,6 +47,8 @@ void registruj() {
     char* meno;
     char* heslo;
     char* potvrdenie;
+    char buffPrihlaseny[128];
+    bool jePrihlaseny = false;
 
     printf("\n");
     printf("Zadajte nove meno: \n");
@@ -40,23 +59,40 @@ void registruj() {
     scanf("%s", &potvrdenie);
 
     if(strcmp(&heslo, &potvrdenie) == 0) {
-        printf("Registracia uspesna!!\n");
-
         FILE* subor;
 
         subor = fopen("prihlaseny.txt", "a+");
 
-        fprintf(subor, &meno);
+        while (fscanf(subor, "%s %*s", buffPrihlaseny) != EOF) {
+            if(strcmp(buffPrihlaseny, &meno) == 0) {
+                printf("Pouzivatel s tymto loginom uz existuje!\n");
+                jePrihlaseny = true;
+                break;
+            } else {
+                break;
+            }
+        }
 
+        if(jePrihlaseny) {
+            registruj(socket);
+        } else {
+
+            fprintf(subor, "%s", &meno);
+            fprintf(subor, " ");
+            fprintf(subor, "%s", &heslo);
+            fprintf(subor, "\n");
+            printf("Registracia uspesna!!\n");
+        }
 
         fclose(subor);
+
     } else {
         printf("Hesla nie su rovnake!!\n");
         registruj();
     }
 }
 
-void autentifikacia() {
+void autentifikacia(int socket) {
     printf("Zadajte volbu, co chcete aby sa stalo:\n");
     printf("1 -- prihlasenie\n");
     printf("2 -- registracia\n");
@@ -65,15 +101,14 @@ void autentifikacia() {
 
     scanf("%d", &volba);
 
-
+    write(socket, "test", strlen("test"));
     if(volba == 1) {
         prihlas();
     } else if(volba == 2) {
-        registruj();
-    } else {
-        autentifikacia();
+        registruj(socket);
+    } else if(volba == 3) {
+        exit(5);
+    }else {
+            autentifikacia(socket);
+        }
     }
-}
-
-
-
